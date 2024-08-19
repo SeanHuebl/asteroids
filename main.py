3564cd951f64765be2a1b0d4bf170e8562e3d6cd
SeanH <sean.huebl@gmail.com>
2024-08-19 18:55:15 +0000

    init pygame and screen added, will need to finish on local machine instead of codespaces since project requires GPU rendering

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
-    print(SCREEN_WIDTH)
-    print(SCREEN_HEIGHT)
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
 
 
 if __name__ == "__main__":
