f366c76ddf7e0196ee8e1065d89a8498835f1720
SeanH <85364282+SeanHuebl@users.noreply.github.com>
2024-09-22 17:51:42 +0000

    Learned and implemented pygame groups and iterated over the groups in the game loop isntead of manually calling update and draw for the player. This improves scalability when needing to draw multiple things to the screen while maintaining clean readable code.

--- main.py
+++ main.py
@@ -8,15 +8,21 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color='black')
-        player.update(dt)
-        player.draw(screen)
+        for _ in updateable:
+            _.update(dt)
+        for _ in drawable:
+            _.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 if __name__ == "__main__":
